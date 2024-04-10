9485a82d9cccffc022cc094b83d4997aa2b51cbf
kanshi <46557+kanshi@users.noreply.github.com>
2024-04-10 15:26:42 +0200

    Updates to handling secrets

--- scripts/generate-config.ts
+++ scripts/generate-config.ts
@@ -7,7 +7,7 @@ const networkName = process.env.NETWORK_NAME || 'NETWORK_NAME-not-set'
 const phaseName = process.env.PHASE_NAME || 'PHASE_NAME-not-set'
 
 const vault = require('node-vault')({
-    apiVersion: 'v1',
+    pathPrefix: '/kv/data/',
     endpoint: process.env.VAULT_URI || 'http://127.0.0.1:8200',
     token: process.env.VAULT_TOKEN || 'invalid-token'
 })
@@ -15,39 +15,50 @@ const vault = require('node-vault')({
 const provider = new ethers.providers.JsonRpcProvider(process.env.JSON_RPC || 'JSON_RPC-not-set')
 
 function makePath(root: string, network: string, phase: string) {
-    return `kv/${root}/${network}/${phase}`
+    return `${root}/${network}/${phase}`
 }
 
-async function generateKeyPair(contractName: string, secretPrefix: string) {
+async function generateDeployer(contractName: string, secretPrefix: string) {
     const wallet = new ethers.Wallet(ethers.Wallet.createRandom(), provider)
 
-    const vaultKeyPath = `${makePath(contractName, networkName, phaseName)}/${secretPrefix}_KEY`
-    const vaultAddressPath = `${makePath(contractName, networkName, phaseName)}/${secretPrefix}_ADDRESS`
-    
+    const vaultPath = makePath(contractName, networkName, phaseName)
+    const keySecret = `${secretPrefix}_DEPLOYER_KEY`
+    const addressSecret = `${secretPrefix}_DEPLOYER_ADDRESS`
     if(isLive) {
-        await vault.write(vaultKeyPath, { value: wallet.privateKey })
-        console.log(`Stored ${vaultKeyPath}`)
-        await vault.write(vaultAddressPath, { value: wallet.address })
-        console.log(`Stored ${vaultAddressPath}`)
-    } else console.log(`Not live. Skipping set up of\n VAULT:${vaultKeyPath} => ${wallet.privateKey}\n VAULT:${vaultAddressPath} => ${wallet.address}`)
+        await vault.write(vaultPath, `{ "data": { "${keySecret}": "${wallet.privateKey}", "${addressSecret}": "${wallet.address}" } }`)
+        console.log(`Stored ${vaultPath}`)
+    } else console.log(`Not live. Skipping ${vaultPath}`)
 
     return wallet.address
 }
 
+async function generateDeployerAndOperator(contractName: string, secretPrefix: string) {
+    const deployer = new ethers.Wallet(ethers.Wallet.createRandom(), provider)
+    const operator = new ethers.Wallet(ethers.Wallet.createRandom(), provider)
+
+    const vaultPath = makePath(contractName, networkName, phaseName)
+    const deployerKeySecret = `${secretPrefix}_DEPLOYER_KEY`
+    const deployerAddressSecret = `${secretPrefix}_DEPLOYER_ADDRESS`
+    const operatorKeySecret = `${secretPrefix}_OPERATOR_KEY`
+    const operatorAddressSecret = `${secretPrefix}_OPERATOR_ADDRESS`
+    if(isLive) {
+        await vault.write(vaultPath, `{ "data": { "${deployerKeySecret}": "${deployer.privateKey}", "${deployerAddressSecret}": "${deployer.address}", "${operatorKeySecret}": "${operator.privateKey}", "${operatorAddressSecret}": "${operator.address}" } }`)
+        console.log(`Stored ${vaultPath}`)
+    } else console.log(`Not live. Skipping ${vaultPath}`)
+
+    return [deployer.address, operator.address]
+}
+
 async function main() {
-    console.log(`Starting with LIVE: ${isLive} in:${phaseName} network:${networkName}`)
+    console.log(`Starting with LIVE:${isLive} in:${phaseName} network:${networkName}`)
     if (isLive) {
         console.log('Waiting for 30s before starting live operations!')
         await new Promise(r => setTimeout(r, 30000));
     }
     
-    const atorTokenDeployerAddress = await generateKeyPair('ator-token', 'TOKEN_DEPLOYER')
-
-    const registratorDeployerAddress = await generateKeyPair('registrator', 'REGISTRATOR_DEPLOYER')
-    const registratorOperatorAddress = await generateKeyPair('registrator', 'REGISTRATOR_OPERATOR')
-
-    const facilitatorDeployerAddress = await generateKeyPair('facilitator', 'FACILITATOR_DEPLOYER')
-    const facilitatorOperatorAddress = await generateKeyPair('facilitator', 'FACILITATOR_OPERATOR')
+    let atorTokenDeployerAddress = await generateDeployer('ator-token', 'TOKEN')
+    let [registratorDeployerAddress, registratorOperatorAddress] = await generateDeployerAndOperator('registrator', 'REGISTRATOR')
+    let [facilitatorDeployerAddress, facilitatorOperatorAddress] = await generateDeployerAndOperator('facilitator', 'FACILITATOR')
 
     const adminWallet = new ethers.Wallet(process.env.DEPLOY_ADMIN_KEY || 'DEPLOY_ADMIN_KEY-not-set', provider)
     console.log(`Loaded deploy admin wallet ${adminWallet.address}`)
